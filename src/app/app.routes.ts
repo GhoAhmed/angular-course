0fb391594caac5852c3f7ab0226e0ac7b849ac06
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2025-10-28 19:07:59 +0100

    structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HelloComponent } from './components/sections/hello/hello.component';
+import { PropertiesComponent } from './components/sections/properties/properties.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'hello', component: HelloComponent },
+  { path: 'properties', component: PropertiesComponent },
+  { path: '**', redirectTo: '' },
+];
